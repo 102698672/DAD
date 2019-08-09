7af3afad7a2ad3968c804e6334d1fc67ba5a8ad8
102698672 <48079141+102698672@users.noreply.github.com>
2019-08-09 10:44:06 +1000

    Task 11 Commit

---
+++ week_2_tasks.sql
@@ -0,0 +1,82 @@
+drop table if exists PlayerRegistration;
+drop table if exists TeamEntry;
+drop table if exists Club;
+drop table if exists Season;
+drop table if exists Player;
+
+CREATE TABLE Player (
+PlayerID INT NOT NULL, 
+Lname NVARCHAR (100) NOT NULL,
+FName NVARCHAR (100) NOT NULL, 
+Phone NVARCHAR (50) ,
+Primary Key (PlayerID)
+);
+
+INSERT INTO Player (
+PlayerID, 
+Fname, 
+LName, 
+Phone) 
+
+Values 
+(10002, 'John', 'Howard', 5552345), 
+(10003, 'Julia', 'Gillard', 5553456);
+
+CREATE TABLE Club (
+ClubName NVARCHAR (100), 
+ContactName NVARCHAR (100),
+Primary Key (ClubName)
+);
+
+INSERT INTO Club (ClubName, ContactName) 
+
+Values 
+('Mt Martha Basketball Club', 'Bob Jane');
+
+CREATE TABLE Season (
+SeasonYear INT, 
+SeasonName NVARCHAR (6), 
+ 
+Primary Key (SeasonYear, SeasonName),
+);
+
+INSERT INTO Season (SeasonYear, SeasonName) 
+
+Values 
+(2018, 'Winter'),
+(2018, 'Summer');
+
+CREATE TABLE TeamEntry (
+ClubName NVARCHAR (100), 
+SeasonYear INT,
+SeasonName NVARCHAR (6),
+AgeGroup NVARCHAR (3), 
+TeamNumber INT,
+
+Primary Key (ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber),
+Foreign Key (ClubName) References Club,
+Foreign Key (SeasonYear, SeasonName) References Season
+);
+
+INSERT INTO TeamEntry (ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber)Values
+('Mt Martha Basketball Club', 2018, 'Summer', 'U14', 1),
+('Mt Martha Basketball Club', 2018, 'Summer', 'U14', 2);
+
+CREATE TABLE PlayerRegistration (
+PlayerID INT, 
+ClubName NVARCHAR (100),
+SeasonYear INT,
+SeasonName NVARCHAR (6),
+AgeGroup NVARCHAR (3), 
+TeamNumber INT,
+DateRegistered DATE,
+
+Primary Key (PlayerID, ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber, DateRegistered),
+Foreign Key (ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber) References TeamEntry,
+
+);
+
+INSERT INTO PlayerRegistration (PlayerID, ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber, DateRegistered)Values
+(10003, 'Mt Martha Basketball Club', 2018,'Summer', 'U14', 2, '12/05/2019');
+
+SELECT * FROM TeamEntry
